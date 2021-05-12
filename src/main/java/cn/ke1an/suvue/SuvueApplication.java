a9972b34dcf75d8cb26b0ef81fdab01ca1160601
keyan <zhaoky@zkpsych.com>
2021-05-12 21:03:49 +0800

    feat: 项目初始化

---
+++ src/main/java/cn/ke1an/suvue/SuvueApplication.java
@@ -0,0 +1,11 @@
+package cn.ke1an.suvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuvueApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SuvueApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/ke1an/suvue/SuvueApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ke1an.suvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuvueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
