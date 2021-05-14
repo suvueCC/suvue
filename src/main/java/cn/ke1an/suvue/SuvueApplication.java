be5d94079fe0ce8752aacf9ce3504660b9ae412d
keyan <zhaoky@zkpsych.com>
2021-05-14 23:30:47 +0800

    feat: 集成mybatis-plus  替换掉mybatis

--- src/main/java/cn/ke1an/suvue/SuvueApplication.java
+++ src/main/java/cn/ke1an/suvue/SuvueApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/05/12 21:09
  */
 @SpringBootApplication
-@MapperScan(basePackages = {"cn.ke1an.suvue.mapper"})
 public class SuvueApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuvueApplication.class, args);
--- src/test/java/cn/ke1an/suvue/mapper/UserMapperTest.java
+++ src/test/java/cn/ke1an/suvue/mapper/UserMapperTest.java
@@ -9,10 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -25,7 +25,7 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserMapperTest {
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     @Test
